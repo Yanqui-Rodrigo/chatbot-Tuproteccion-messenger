97c656b12a0f6a624b8f9651f71b379dba393a21
Yanqui-Rodrigo <rodyanquic@upt.pe>
2021-06-30 06:47:28 -0500

    agregado mensaje de respuesta mascarilla7

--- Facebook/facebookBot.js
+++ Facebook/facebookBot.js
@@ -333,15 +333,15 @@ switch(action){
         }
         if(colorMascarilla==="blanco"){
           var total = cantidadMascarilla*7;
-          let userData = getUserData(sender);
+          let userData2 = getUserData(sender);
           sendTextMessage(sender, "Usted debe pagar "+total+" soles de mascarillas color "+colorMascarilla);
-          axios.post("https://sheet.best/api/sheets/22ea581b-8ea8-4ea9-a10f-8747fd6dab75",{"id":idtransaccion,"Nombres":userData.first_name,"Producto":"Mascarilla","Tipo":colorMascarilla,"Cantidad":cantidadMascarilla,"Precio":"6","Total":total,"Estado":"Pendiente"});
+          axios.post("https://sheet.best/api/sheets/22ea581b-8ea8-4ea9-a10f-8747fd6dab75",{"id":idtransaccion,"Nombres":userData2.first_name,"Producto":"Mascarilla","Tipo":colorMascarilla,"Cantidad":cantidadMascarilla,"Precio":"6","Total":total,"Estado":"Pendiente"});
         }
         if(colorMascarilla==="negro"){
           var total = cantidadMascarilla*10;
-          let userData = await getUserData(recipientId);
+         // let userData = await getUserData(recipientId);
           sendTextMessage(sender, "Usted debe pagar "+total+" soles de mascarillas color "+colorMascarilla);
-          axios.post("https://sheet.best/api/sheets/22ea581b-8ea8-4ea9-a10f-8747fd6dab75",{"id":idtransaccion,"Nombres":userData.last_name,"Producto":"Mascarilla","Tipo":colorMascarilla,"Cantidad":cantidadMascarilla,"Precio":"6","Total":total,"Estado":"Pendiente"});
+          axios.post("https://sheet.best/api/sheets/22ea581b-8ea8-4ea9-a10f-8747fd6dab75",{"id":idtransaccion,"Nombres":"asdasd","Producto":"Mascarilla","Tipo":colorMascarilla,"Cantidad":cantidadMascarilla,"Precio":"6","Total":total,"Estado":"Pendiente"});
         }
        
       }
