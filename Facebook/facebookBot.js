64e3bf919e14a9a6dc6c251088be589ff1293a2b
Yanqui-Rodrigo <rodyanquic@upt.pe>
2021-06-30 16:21:13 -0500

    prueba con nombre 2

--- Facebook/facebookBot.js
+++ Facebook/facebookBot.js
@@ -340,9 +340,9 @@ switch(action){
         }
         if(colorMascarilla==="negro"){
           var total = cantidadMascarilla*10;
-         let userData =  await getUserData(senderId);
+         //let userData =  getUserData(senderId);
           sendTextMessage(sender, "Usted debe pagar "+total+" soles de mascarillas color "+colorMascarilla);
-          axios.post("https://sheet.best/api/sheets/22ea581b-8ea8-4ea9-a10f-8747fd6dab75",{"id":idtransaccion,"Nombres":userData.first_name,"Producto":"Mascarilla","Tipo":colorMascarilla,"Cantidad":cantidadMascarilla,"Precio":"6","Total":total,"Estado":"Pendiente"});
+          axios.post("https://sheet.best/api/sheets/22ea581b-8ea8-4ea9-a10f-8747fd6dab75",{"id":idtransaccion,"Nombres":senderid,"Producto":"Mascarilla","Tipo":colorMascarilla,"Cantidad":cantidadMascarilla,"Precio":"6","Total":total,"Estado":"Pendiente"});
         }
        
       }
